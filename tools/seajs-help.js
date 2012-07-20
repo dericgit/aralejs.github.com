d6613e6e0248c79933634947e20bcc2f41009ea4
lepture <lepture@me.com>
2012-07-20 09:20:21 +0800

    update tools

--- tools/seajs-help.js
+++ tools/seajs-help.js
@@ -19,7 +19,7 @@
         '/$1/src/$2'
     )
     if (url.indexOf('src') < 0 || url.indexOf('dist') < 0) {
-        url.replace(githubBase, '../src/')
+        url = url.replace(githubBase, '../src/')
     }
     return url;
   })
