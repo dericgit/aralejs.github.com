5f00ee1535d9142597a8d07ce6c965351ecb05d4
lepture <lepture@me.com>
2012-07-30 14:24:07 +0800

    seprate js. more links

---
+++ assets/home.js
@@ -0,0 +1,35 @@
+define(['#jquery/1.7.2/jquery'], function(require, exports) {
+    var $ = require('#jquery/1.7.2/jquery');
+
+    $('#nav a').click(function(e) {
+        var href = $(this).attr('href');
+        if (href.indexOf('#') === 0) {
+            e.preventDefault();
+            history.pushState && history.pushState(null, null, href);
+            $('html, body').animate({scrollTop: $(href).offset().top}, 400);
+        }
+    });
+
+    var $modules = $('.modules');
+    $modules.hide();
+    if (location.hash == '#utilities' || location.hash == '#widgets') {
+        $(location.hash).show();
+    } else {
+        $('#infrastructure').show();
+    }
+
+    $('.tab a').click(function(e) {
+        e.preventDefault();
+        switchTab($(this).attr('href'));
+        history.pushState && history.pushState(null, null, this.href);
+    });
+
+    function switchTab(id) {
+        var $target = $(id);
+        if ($target.is(':visible')) {
+            return;
+        }
+        $modules.slideUp('ease-in');
+        $target.slideDown('ease-out');
+    }
+});
