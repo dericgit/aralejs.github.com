08d228243b63b77a7d6e3e9b66dd5989a59d89e2
lepture <lepture@me.com>
2012-08-20 19:55:02 +0800

    fix seajs helper

--- tools/seajs-helper.js
+++ tools/seajs-helper.js
@@ -25,7 +25,7 @@
     url = url.replace(/\/([\d.]+)\/([^\/]+\.js)$/, '/$1/dist/$2')
 
     // 如果访问 alipay.im 则从 git.alipay.im 加载
-    if (location.hostname.indexOf('alipay.im') > 0) {
+    if (location.hostname.indexOf('alipay.im') != -1 && url.indexOf(GITHUB_BASE) != -1) {
       // 链接转换成 http://git.alipay.im/overlay/0.9.9/dist/overlay.js
       url = url.replace(GITHUB_BASE, 'http://git.alipay.im/')
 
