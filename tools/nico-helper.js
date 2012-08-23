34853970b058dafb8c903def43a961960b993171
pianyou <afcstar@163.com>
2012-08-23 20:46:09 +0800

    update

--- tools/nico-helper.js
+++ tools/nico-helper.js
@@ -2,7 +2,7 @@ seajs.use('$', function() {
     if (location.hostname.indexOf('.im') > 0) {
         // 更改源码的对应地址
         var code = $('#code-link');
-        code.attr('href', code.attr('href')replace('https://github.com/aralejs', 'http://git.alipay.im'));
+        code.attr('href', code.attr('href').replace('https://github.com/aralejs', 'http://git.alipay.im'));
 
         // 将用例的链接设为新窗口打开
         $('#nav-test').attr('target', '_blank');
