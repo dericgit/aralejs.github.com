d5bd6e8cf58a2689f7ef12c253dca0ebc214a206
pianyou <afcstar@163.com>
2012-08-23 22:30:26 +0800

    update

--- tools/nico-helper.js
+++ tools/nico-helper.js
@@ -19,8 +19,10 @@ seajs.use(['https://a.alipayobjects.com/static/arale/jquery/1.7.2/jquery.js'], f
         */
 
         // 增加一个全局导航触发点
-        $('#main .container').append($('<div class="document-index"><input type="text" id="search" /></div>'))
+        $('#main .container').append($(
+            '<div class="document-index"><input placeholder="搜索组件" type="text" id="search" /></div>'));
 
+        // 载入组件信息
         $.get('/info.json', function(data) {
             $(['arale', 'alipay', 'alice']).each(function(i, item) {
                 var html = '<h2>' + item + '</h2><ul class="document-section fn-clear">',
@@ -38,5 +40,14 @@ seajs.use(['https://a.alipayobjects.com/static/arale/jquery/1.7.2/jquery.js'], f
                 $('.document-index').append($(html));
             });
         }, 'json');
+
+        // 搜索功能
+        $('#search').on('keyup', function(e) {
+            var str = $(this).val();
+            $('.document-index li a').each(function(i, item) {
+                item = $(item);
+                item[ item.html().indexOf(str) !== -1) ? 'show' : 'hide' ]();    
+            }); 
+        });
     }
 });
