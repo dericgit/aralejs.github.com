3f504442f32e7af5ba0f55862866e9c1837c6832
pianyou <afcstar@163.com>
2012-08-23 20:39:40 +0800

    样式微调

--- tools/nico-helper.js
+++ tools/nico-helper.js
@@ -1,6 +1,10 @@
-;(function() {
-  if (location.hostname.indexOf('.im') > 0) {
-    var code = document.getElementById('code-link')
-    code.href = code.href.replace('https://github.com/aralejs', 'http://git.alipay.im')
-  }
-})()
+seajs.use('$', function() {
+    if (location.hostname.indexOf('.im') > 0) {
+        // 更改源码的对应地址
+        var code = $('#code-link');
+        code.attr('href', code.attr('href')replace('https://github.com/aralejs', 'http://git.alipay.im'));
+
+        // 将用例的链接设为新窗口打开
+        $('#nav-test').attr('target', '_blank');
+    }
+});
