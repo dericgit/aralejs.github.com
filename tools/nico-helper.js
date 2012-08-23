3a88ed0d1f60aca71f9e4f233575200b73a39b98
pianyou <afcstar@163.com>
2012-08-23 21:10:48 +0800

    update

--- tools/nico-helper.js
+++ tools/nico-helper.js
@@ -13,6 +13,6 @@ seajs.use(['https://a.alipayobjects.com/static/arale/jquery/1.7.2/jquery.js'], f
         });
 
         // 增加一个全局导航触发点
-        $('#nav').append($('<input type="text" id="search" />'))
+        $('#main .container').append($('<input type="text" id="search" />'))
     }
 });
