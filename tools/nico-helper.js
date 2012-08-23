e058ef25e952d41f62a9108a64ec1410f5ee5fc5
pianyou <afcstar@163.com>
2012-08-23 21:52:31 +0800

    update

--- tools/nico-helper.js
+++ tools/nico-helper.js
@@ -8,9 +8,11 @@ seajs.use(['https://a.alipayobjects.com/static/arale/jquery/1.7.2/jquery.js'], f
         $('#nav-test').attr('target', '_blank');
 
         // 去掉gitlab下的icon
+        /*
         $('#nav a').each(function(index, item) {
             item.className = '';
         });
+        */
 
         // 增加一个全局导航触发点
         $('#main .container').append($('<div class="document-index"><input type="text" id="search" /></div>'))
