323d85755662b72a7e76851e2742ca67da47a1c8
pianyou <afcstar@163.com>
2012-08-23 22:04:28 +0800

    update

--- tools/nico-helper.js
+++ tools/nico-helper.js
@@ -16,5 +16,22 @@ seajs.use(['https://a.alipayobjects.com/static/arale/jquery/1.7.2/jquery.js'], f
 
         // 增加一个全局导航触发点
         $('#main .container').append($('<div class="document-index"><input type="text" id="search" /></div>'))
+
+        $.get('/info.json', function(data) {
+            $(['arale', 'alipay', 'alice']).each(function(i, item) {
+                var html = '<div class="document-setion ' + item + '">',
+                    projects = data[item];
+                for(var i=0; i<projects.length; i++) {
+                    var name = (item === 'arale') ? projects[i].name : item + '.' + projects[i].name;
+                    html += '<li><a href="http://';
+                    html += location.hostname + '/' + name;
+                    html += '">';
+                    html += projects[i].name;
+                    html += '</a><li>';
+                }
+                html += '</div>';
+                $('.document-index').append($(html));
+            });
+        });
     }
 });
